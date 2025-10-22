9289ce8c17fd6a1b370bad1838d8751a1ce6ddba
aqshal004 <aqshalcamp@gmail.com>
2025-10-22 13:24:54 +0700

    model

---
+++ lib/Tugas9/models/electronik.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ElectronikModel {
+  final String name;
+  final int price;
+  final String description;
+  final String image;
+
+  ElectronikModel({
+    required this.name,
+    required this.price,
+    required this.image,
+    required this.description,
+  });
+}
